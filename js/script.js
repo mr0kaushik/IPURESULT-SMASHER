fbe79a80ad2756f106bd4e308687084fdd7c8419
Deepak Kaushik <kaushikd875@gmail.com>
2019-04-13 11:07:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 
 $(document).ready(function (){
+    
     $(searchbtn).click(function(e){
             e.preventDefault();
-            var inputRoll = $('input[name=enrollment-no').val();
-            $.ajax({
+            var inputRoll = $('input[name=enrollment-no]').val();
+                $.ajax({
                 type: "POST",
                 url: "getresult.php",
                 data: {
@@ -12,18 +13,10 @@ $(document).ready(function (){
                 },
                 dataType : "text",
                 success : function(response) {
-                    /*
-                    var peopleHTML = "";
-                    for(var key in response){
-                        peopleHTML += "<tr>";
-                            peopleHTML += "<td>" + response[key]["Roll_Number"] + "<td>";
-                            peopleHTML += "<td>" + response[key]["Name"] + "<td>";
-                        peopleHTML += "<tr>";
-                    }
-                    */
                     $("#userdata").html(response);
                 }
             });
+            
     });
 
     $('input[name=enrollment-no').keypress(function(e){
@@ -31,4 +24,7 @@ $(document).ready(function (){
             $(searchbtn).click();
         }
     });
+
 });
+
+
