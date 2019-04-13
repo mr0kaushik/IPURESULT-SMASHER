7ec0a2f3f790d12bcf5595b75d06365bbfc69222
Deepak Kaushik <kaushikd875@gmail.com>
2019-04-13 11:01:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-    	<meta name="viewport" content="width=device-width, intial-scale=1.0">
+    	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
         <title>IPURESULT SMASHER</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-      	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
       	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
       	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
@@ -41,19 +40,16 @@
             <div class="row text-center padding">
               <div class="col-md-12">
                     <h2>GET YOUR SEMESTER RESULT HERE</h2>
-                    <form class="enrollment-form" onsubmit="return getCall" method="post">
-                        <input type="text" name="enrollment-no" id="inputRoll" placeholder="Enter Enrollment No."
-                        pattern="[0-9]{11}" title="Please enter valid enrollment number.">
+                    <form class="enrollment-form" method="post">
+                        <input type="text" name="enrollment-no" id="inputRoll" placeholder="Enter Enrollment No.">
                         <button type="submit" id="searchbtn" name="button">GET RESULT</button>
                     </form>
               </div>
             </div>
 
             <div class="row">
-              <div class="col-md-12">
-                <div id="userdata">
-
-                </div>
+              <div class="col-md-12" id="userdata">
+                
               </div>
             </div>
 
