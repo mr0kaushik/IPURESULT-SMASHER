eef11d9866f638861eea7633107adc5cb64448b5
Deepak Kaushik <kaushikd875@gmail.com>
2019-04-13 11:01:55 +0530

    Update ourteam.php

--- ourteam.php
+++ ourteam.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Our Team</title>
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
@@ -54,7 +56,7 @@
         <div class="col-xs-12 col-sm-6 col-md-4 member">
           <p><img src="images/akash.jpg" alt="Akash Kumar"></p>
           <h4>Akash Kumar</h4>
-          <h5>Content Provider</h5>
+          <h5>Document Parser</h5>
         </div>
       </div>
     </div>
